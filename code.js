98f5dad301b7d96500edc296a2ca9aea976c5f00
rms543221 <re441742@ucf.edu>
2026-01-24 17:21:30 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,159 @@
+const urlBase = "http://143.198.6.35/API";
+const extension = "php";
+
+let userId = 0;
+let firstName = "";
+let lastName = "";
+
+// ---------------- LOGIN ----------------
+function doLogin() {
+    let login = document.getElementById("loginName").value;
+    let password = document.getElementById("loginPassword").value;
+    document.getElementById("loginResult").innerHTML = "";
+
+    let tmp = {login, password};
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", `${urlBase}/Login.${extension}`, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    xhr.onreadystatechange = function() {
+        if(this.readyState === 4) {
+            let resp = JSON.parse(this.responseText);
+            if(resp.id > 0) {
+                userId = resp.id;
+                firstName = resp.firstName;
+                lastName = resp.lastName;
+                saveCookie();
+                window.location.href = "contacts.html";
+            } else {
+                document.getElementById("loginResult").innerHTML = "Invalid credentials";
+            }
+        }
+    };
+    xhr.send(JSON.stringify(tmp));
+}
+
+// ---------------- REGISTER ----------------
+function doRegister() {
+    let firstNameReg = document.getElementById("regFirstName").value;
+    let lastNameReg = document.getElementById("regLastName").value;
+    let loginReg = document.getElementById("regLogin").value;
+    let passwordReg = document.getElementById("regPassword").value;
+
+    let tmp = {firstName:firstNameReg, lastName:lastNameReg, login:loginReg, password:passwordReg};
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", `${urlBase}/Register.${extension}`, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    xhr.onreadystatechange = function() {
+        if(this.readyState === 4) {
+            let resp = JSON.parse(this.responseText);
+            if(resp.id > 0) window.location.href = "index.html";
+            else document.getElementById("registerResult").innerHTML = "Registration failed";
+        }
+    };
+    xhr.send(JSON.stringify(tmp));
+}
+
+// ---------------- COOKIE ----------------
+function saveCookie() {
+    let date = new Date();
+    date.setTime(date.getTime() + 20*60*1000);
+    document.cookie = `firstName=${firstName},lastName=${lastName},userId=${userId};expires=${date.toGMTString()}`;
+}
+
+function readCookie() {
+    let data = document.cookie.split(",");
+    userId = -1;
+    data.forEach(d => {
+        let [k,v] = d.trim().split("=");
+        if(k=="firstName") firstName=v;
+        if(k=="lastName") lastName=v;
+        if(k=="userId") userId=parseInt(v);
+    });
+    if(userId<0) window.location.href="index.html";
+}
+
+// ---------------- LOGOUT ----------------
+function doLogout() {
+    userId=0; firstName=""; lastName="";
+    document.cookie="firstName= ; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    window.location.href="index.html";
+}
+
+// ---------------- CREATE ----------------
+function addContact() {
+    let first=document.getElementById("firstName").value;
+    let last=document.getElementById("lastName").value;
+    let phone=document.getElementById("phone").value;
+    let email=document.getElementById("email").value;
+
+    let tmp={firstName:first,lastName:last,phone, email, userId};
+    let xhr=new XMLHttpRequest();
+    xhr.open("POST", `${urlBase}/AddContact.${extension}`, true);
+    xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+
+    xhr.onreadystatechange = function() {
+        if(this.readyState===4) {
+            document.getElementById("contactAddResult").innerHTML="Contact added";
+            searchContacts(); // refresh list
+        }
+    };
+    xhr.send(JSON.stringify(tmp));
+}
+
+// ---------------- READ ----------------
+function searchContacts() {
+    let srch=document.getElementById("searchText").value;
+    let tmp={search:srch,userId};
+    let xhr=new XMLHttpRequest();
+    xhr.open("POST", `${urlBase}/SearchContacts.${extension}`, true);
+    xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+
+    xhr.onreadystatechange=function() {
+        if(this.readyState===4) {
+            let resp=JSON.parse(this.responseText);
+            let listHtml="";
+            resp.results.forEach(c => {
+                listHtml+=`${c.FirstName} ${c.LastName} | ${c.Phone} | ${c.Email} 
+                <button onclick="editContact(${c.ID})">Edit</button> 
+                <button onclick="deleteContact(${c.ID})">Delete</button><br>`;
+            });
+            document.getElementById("contactSearchResult").innerHTML=listHtml;
+        }
+    };
+    xhr.send(JSON.stringify(tmp));
+}
+
+// ---------------- UPDATE ----------------
+function editContact(id) {
+    let first=prompt("Enter first name:");
+    let last=prompt("Enter last name:");
+    let phone=prompt("Enter phone:");
+    let email=prompt("Enter email:");
+    if(first && last && phone && email) {
+        let tmp={id,firstName:first,lastName:last,phone,email,userId};
+        let xhr=new XMLHttpRequest();
+        xhr.open("POST", `${urlBase}/UpdateContact.${extension}`, true);
+        xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+        xhr.onreadystatechange=function() {
+            if(this.readyState===4) searchContacts();
+        };
+        xhr.send(JSON.stringify(tmp));
+    }
+}
+
+// ---------------- DELETE ----------------
+function deleteContact(id) {
+    if(confirm("Delete this contact?")) {
+        let tmp={id,userId};
+        let xhr=new XMLHttpRequest();
+        xhr.open("POST", `${urlBase}/DeleteContact.${extension}`, true);
+        xhr.setRequestHeader("Content-type","application/json; charset=UTF-8");
+        xhr.onreadystatechange=function() {
+            if(this.readyState===4) searchContacts();
+        };
+        xhr.send(JSON.stringify(tmp));
+    }
+}
+
